0a41e865efd56ff43b977b138051e1121e9f2f2d
Alex Fleming <alexfleming421@gmail.com>
2022-04-30 03:11:14 -0500

    prepping filter data

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Index0
+const fs = require('fs');
+const path = require('path');
+const inputPath = path;
+const exportPath = path;
+
+const importFilter = (data) => {
+    
+};
+
+const processFilter = (data) => {
+    
+};
+
+const exportFilter = (data) => {
+    
+};
+
+
+// const data = fs.readFileSync(inputPath, 'utf-8');
