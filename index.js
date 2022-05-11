8c859a8a947b02c83e5262992c4ebf46c37fd2d1
Alex Fleming <alexfleming421@gmail.com>
2022-05-11 00:36:23 -0500

    dynamic parsing and export

--- index.js
+++ index.js
@@ -181,11 +181,13 @@ const filterComponentFilePath = [
     '4800_Leveling_Useful_magic_and_normal_items/4806_Show_All_unknown_Section/4806_Show_All_unknown_Section'
 ];
 
+/*
 const exportFilter = (exportPath, data, extension) => {
     const content = data.join('\n');
     const filterComponentOutputFile = path.join(exportPath, 'Custom_Filter' + extension);
     fs.writeFileSync(filterComponentOutputFile, content, 'utf-8');
 };
+*/
 
 const importFilter = (strictness) => {
     const now = dayjs();
@@ -250,7 +252,7 @@ const matchCommentHeaderName = (data) => {
 };
 
 const matchCommentHeaderNumber = (data) => {
-    const regex = /[0-9]/g;
+    const regex = /[0-9]+/g;
     const matchResults = data.match(regex);
     const headerNumber = matchResults[0];
     
@@ -259,15 +261,49 @@ const matchCommentHeaderNumber = (data) => {
         return parseInt(headerNumber);
     } else {
         return headerNumber;
+    };
+};
+
+const matchCommentHeaderIndex = (lastChapterIndex, lastChapterNumber, chapterNumberList, lastSectionIndex, lastSectionNumber, sectionNumberList) => {
+    // Chapter Variables
+    const chapterIndex = lastChapterIndex;
+    const chapterLength = chapterNumberList.length;
+    const previousChapter = lastChapterNumber;
+    const nextChapter = lastChapterNumber + 100;
+    // Section Variables
+    const sectionIndex = lastSectionIndex;
+    const sectionLength = sectionNumberList.length;
+    const previousSection = lastSectionNumber;
+    const expectedSection = lastChapterNumber + lastSectionIndex +2;
+
+    const tracker = {
+        trackerChapterIndex: chapterIndex,
+        trackerChapterLength: chapterLength,
+        trackerPreviousChapter: previousChapter,
+        trackerNextChapter: nextChapter,
+        trackerSectionIndex: sectionIndex,
+        trackerSectionLength: sectionLength,
+        trackerPreviousSection: previousSection,
+        trackerExpectedSection: expectedSection
     }
+
+    // console.log('Matching Index: ', tracker);
+
+    if (previousSection > previousChapter && previousSection < nextChapter) {
+        return true
+        /*
+        if (previousSection === expectedSection) {
+            return true
+        } else {
+            return false
+        };
+        */
+    } else {
+        return false
+    };
 };
 
 const importAsJSON = () => {
-    const now = dayjs();
-    const folderFormat = now.format('YYYY_MM_DD_HH_mm_ss');
-    const exportPath = path.join(outputPath, `JSON_Custom_Filters_${folderFormat}`);
-    fs.mkdirSync(exportPath);
-
     const filterComponentInputFile = path.join(examplePath, 'custom.filter');
     const filterData = fs.readFileSync(filterComponentInputFile, 'utf-8');
 
@@ -284,12 +320,15 @@ const importAsJSON = () => {
     };
     
     // Tracker Vars
-    let lastChapterTrigger = false;
-    let lastChapterNumber = 0;
-    let lastChapterIndex = 0;
-    let lastSectionTrigger = false;
-    let lastSectionNumber = 0;
-    let lastSectionIndex = 0;
+    var lastChapterTrigger = false;
+    var lastChapterIndex = 0;
+    var lastChapterNumber = 0;
+    var chapterNumberList = [];
+    
+    var lastSectionTrigger = false;
+    var lastSectionIndex = 0;
+    var lastSectionNumber = 0;
+    var sectionNumberList = [];
 
     // Process Filter Data
     rules.forEach(rule => {
@@ -312,23 +351,22 @@ const importAsJSON = () => {
                 const chapterHeaderCommentDecorator = checkString(comment, '#==');
                 const chapterHeaderCommentLabel = checkCommentHeader(comment);
 
-                switch (comment) {
-                    case chapterHeaderCommentDecorator:
-                        chapterHeaderJSON.chapterHeaderCommentLine = comment;
-                        break;
-                    case chapterHeaderCommentLabel:
-                        const commentNumber = matchCommentHeaderNumber(comment);
-                        lastChapterNumber = commentNumber;
-                        chapterHeaderJSON.chapterHeaderNumber = commentNumber;
-                        chapterHeaderJSON.chapterHeaderName = matchCommentHeaderName(comment);
-                        break;
-                    default:
-                        chapterHeaderJSON.chapterHeaderMisc.push(comment);
+                if (chapterHeaderCommentDecorator) {
+                    chapterHeaderJSON.chapterHeaderCommentLine = comment;
+                } else if (chapterHeaderCommentLabel) {
+                    const commentNumber = matchCommentHeaderNumber(comment);
+                    lastChapterNumber = commentNumber;
+                    chapterNumberList.push(lastChapterNumber);
+                    chapterHeaderJSON.chapterHeaderNumber = commentNumber;
+                    chapterHeaderJSON.chapterHeaderName = matchCommentHeaderName(comment);
+                } else {
+                    chapterHeaderJSON.chapterHeaderMisc.push(comment);
                 };
             });
             
             // Push to Filter JSON
             lastChapterTrigger = true;
+            lastSectionIndex = 0;
             filterJSON.chapters.push(chapterHeaderJSON);
 
         } else if (checkString(rule, '#--')) {
@@ -349,18 +387,16 @@ const importAsJSON = () => {
                 const sectionHeaderCommentDecorator = checkString(comment, '#--');
                 const sectionHeaderCommentLabel = checkCommentHeader(comment);
 
-                switch (comment) {
-                    case sectionHeaderCommentDecorator:
-                        sectionHeaderJSON.sectionHeaderCommentLine = comment;
-                        break;
-                    case sectionHeaderCommentLabel:
-                        const commentNumber = matchCommentHeaderNumber(comment);
-                        lastSectionNumber = commentNumber;
-                        sectionHeaderJSON.sectionHeaderNumber = commentNumber;
-                        sectionHeaderJSON.sectionHeaderName = matchCommentHeaderName(comment);
-                        break;
-                    default:
-                        sectionHeaderJSON.sectionHeaderMisc.push(comment);
+                if (sectionHeaderCommentDecorator) {
+                    sectionHeaderJSON.sectionHeaderCommentLine = comment;
+                } else if (sectionHeaderCommentLabel) {
+                    const commentNumber = matchCommentHeaderNumber(comment);
+                    lastSectionNumber = commentNumber;
+                    sectionNumberList.push(lastSectionNumber);
+                    sectionHeaderJSON.sectionHeaderNumber = commentNumber;
+                    sectionHeaderJSON.sectionHeaderName = matchCommentHeaderName(comment);
+                } else {
+                    sectionHeaderJSON.sectionHeaderMisc.push(comment);
                 };
             });
 
@@ -382,39 +418,137 @@ const importAsJSON = () => {
                 const ruleEnabledShow = checkString(property, 'show');
                 const ruleEnabledHide = checkString(property, 'hide');
 
-                switch (property) {
-                    case ruleDisabledShow:
-                        ruleJSON.disabled = true;
-                        ruleJSON.visible = 'show';
-                        break;
-                    case ruleDisabledHide:
-                        ruleJSON.disabled = true;
-                        ruleJSON.visible = 'hide';
-                        break;
-                    case ruleEnabledShow:
-                        ruleJSON.disabled = false;
-                        ruleJSON.visible = 'show';
-                        break;
-                    case ruleEnabledHide:
-                        ruleJSON.disabled = false;
-                        ruleJSON.visible = 'hide';
-                        break;
-                    default:
-                        ruleJSON.propertyMisc.push(property);
+                // Debugging
+                // console.log('Rule Property Parsing...');
+
+                if (ruleDisabledShow) {
+                    ruleJSON.disabled = true;
+                    ruleJSON.visible = 'show';
+                } else if (ruleDisabledHide) {
+                    ruleJSON.disabled = true;
+                    ruleJSON.visible = 'hide';
+                } else if (ruleEnabledShow) {
+                    ruleJSON.disabled = false;
+                    ruleJSON.visible = 'show';
+                } else if (ruleEnabledHide) {
+                    ruleJSON.disabled = false;
+                    ruleJSON.visible = 'hide';
+                } else {
+                    ruleJSON.propertyMisc.push(property);
                 };
             });
 
             // Push to Filter JSON
-            if (lastSectionNumber === lastChapterNumber + lastSectionIndex + 1) {
-                filterJSON.chapters[lastChapterIndex].chapterSections[lastSectionIndex].rules.push(ruleJSON);
+            if (matchCommentHeaderIndex(lastChapterIndex, lastChapterNumber, chapterNumberList, lastSectionIndex, lastSectionNumber, sectionNumberList)) {
+                try {
+                    // console.log('Section Last Index: ', lastSectionIndex);
+                    // console.log('Section Length: ', filterJSON.chapters[lastChapterIndex].chapterSections.length);
+                    // console.log('Sections: ', filterJSON.chapters[lastChapterIndex].chapterSections);
+                    filterJSON.chapters[lastChapterIndex].chapterSections[lastSectionIndex-2].rules.push(ruleJSON);
+                } catch (error) {
+                    // If undefined section, apply to chapter rules
+                    filterJSON.chapters[lastChapterIndex].rules.push(ruleJSON);
+                }
             } else {
                 filterJSON.chapters[lastChapterIndex].rules.push(ruleJSON);
             }
         };
     });
 
-    const filterComponentOutputFile = path.join(exportPath, 'JSON_Custom_Filter.txt');
-    fs.writeFileSync(filterComponentOutputFile, JSON.stringify(filterJSON, null, 4), 'utf-8');
+    convertJSONtoFilter(filterJSON);
+    // fs.writeFileSync(filterComponentOutputFile, JSON.stringify(filterJSON, null, 4), 'utf-8');
+};
+
+const convertJSONtoFilter = (data) => {
+    const filterData = [];
+
+    for (let chapter = 0; chapter < data.chapters.length; chapter++) {
+        // Add leading zeros
+        const chapterNumber = data.chapters[chapter].chapterHeaderNumber;
+        let chapterNumberString = '';
+        if (chapterNumber < 1000) {
+            chapterNumberString = `0${chapterNumber}`;
+        } else {
+            chapterNumberString = `${chapterNumber}`;
+        }
+
+        // Build Chapter Headers
+        filterData.push(`${data.chapters[chapter].chapterHeaderCommentLine}`);
+        filterData.push(`# [[${chapterNumberString}]] ${data.chapters[chapter].chapterHeaderName}`);
+        filterData.push(`${data.chapters[chapter].chapterHeaderCommentLine}`);
+
+        // Add Chapter Misc
+        if (data.chapters[chapter].chapterHeaderMisc.length) {
+            const chapterMisc = data.chapters[chapter].chapterHeaderMisc;
+            for (let misc = 0; misc < chapterMisc.length; misc++) {
+                filterData.push(`${data.chapters[chapter].chapterHeaderMisc[misc]}`);
+            }
+            filterData.push('');
+        } else {
+            filterData.push('\n');
+        }
+
+        // Build Chapter Rules
+        if (data.chapters[chapter].rules.length) {
+            const chapterRules = data.chapters[chapter].rules;
+            for (let rule = 0; rule < chapterRules.length; rule++) {
+                const chapterRuleProperties = data.chapters[chapter].rules[rule].propertyMisc;
+                filterData.push(`${chapterRuleProperties.join('\n\t')}`);
+                filterData.push('');
+            }
+        }
+
+        if (data.chapters[chapter].chapterSections.length) {
+            for (let section = 0; section < data.chapters[chapter].chapterSections.length; section++) {
+                // Add leading zeros
+                const sectionNumber = data.chapters[chapter].chapterSections[section].sectionHeaderNumber;
+                let sectionNumberString = '';
+                if (chapterNumber < 1000) {
+                    sectionNumberString = `0${sectionNumber}`;
+                } else {
+                    sectionNumberString = `${sectionNumber}`;
+                }
+
+                // Build Section Headers
+                filterData.push(`${data.chapters[chapter].chapterSections[section].sectionHeaderCommentLine}`);
+                filterData.push(`#   [${sectionNumberString}] ${data.chapters[chapter].chapterHeaderName}`);
+                filterData.push(`${data.chapters[chapter].chapterSections[section].sectionHeaderCommentLine}`);
+        
+                // Add Section Misc
+                if (data.chapters[chapter].chapterSections[section].sectionHeaderMisc.length) {
+                    const sectionMisc = data.chapters[chapter].chapterSections[section].sectionHeaderMisc;
+                    for (let misc = 0; misc < sectionMisc.length; misc++) {
+                        filterData.push(`${data.chapters[chapter].chapterSections[section].sectionHeaderMisc[misc]}`);
+                    }
+                    filterData.push('');
+                } else {
+                    filterData.push('\n');
+                }
+
+                // Build Section Rules
+                if (data.chapters[chapter].chapterSections[section].rules.length) {
+                    const sectionRules = data.chapters[chapter].chapterSections[section].rules;
+                    for (let rule = 0; rule < sectionRules.length; rule++) {
+                        const sectionRuleProperties = data.chapters[chapter].chapterSections[section].rules[rule].propertyMisc;
+                        filterData.push(`${sectionRuleProperties.join('\n\t')}`);
+                        filterData.push('');
+                    }
+                }
+            }
+        }
+    }
+    exportFilter(data, filterData.join('\n'));
+};
+
+const exportFilter = (json, filterData) => {
+    const now = dayjs();
+    const folderFormat = now.format('YYYY_MM_DD_HH_mm_ss');
+    const exportPath = path.join(outputPath, `Exported_Filters_${folderFormat}`);
+    const filterJSONOutputFile = path.join(exportPath, 'Exported_Custom.json');
+    const filterOutputFile = path.join(exportPath, 'Exported_Custom.filter');
+    fs.mkdirSync(exportPath);
+    fs.writeFileSync(filterJSONOutputFile, JSON.stringify(json, null, 4), 'utf-8');
+    fs.writeFileSync(filterOutputFile, filterData, 'utf-8');
 };
 
 const main = () => {
