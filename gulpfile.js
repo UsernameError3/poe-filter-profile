109b39f5cf8b5f8d959faa22b87f1c1ed35343d1
UsernameError3 <usernameerror3@outlook.com>
2023-12-30 03:34:56 -0600

    Adding Commit Status

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const exec = require('child_process').execSync;
 
 const build = (cb) => {
     console.log('Fetching Remote Resources...');
@@ -13,4 +14,15 @@ const build = (cb) => {
     return cb()
 }
 
+const commitStatus = (cb) => {
+    let out = exec('git diff --stat');
+    out = out.toString();
+    if (out.indexOf('.json') > -1) {
+        console.log('Build generated new files, aborting commit...');
+        process.exit(1);
+    }
+    cb();
+}
+
 gulp.task('build', build);
+gulp.task('commit-status', commitStatus);
