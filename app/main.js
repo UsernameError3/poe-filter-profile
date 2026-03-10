ee200945df4fabfdd0e509821943633a807e2da6
Alex Fleming <UsernameError3@outlook.com>
2026-03-10 00:50:42 -0500

    mirage updates

--- app/main.js
+++ app/main.js
@@ -23,7 +23,7 @@ const hexToRGB = (hex) => {
         const r = parseInt(hex.slice(1, 3), 16);
         const g = parseInt(hex.slice(3, 5), 16);
         const b = parseInt(hex.slice(5, 7), 16);
-        const a = parseInt(hex.slice(7, 9), 16) / 255;
+        const a = parseInt(hex.slice(7, 9), 16);
 
         // Always return RGBA Value
         return `${r} ${g} ${b} ${a}`;
